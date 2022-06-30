9a61d769daa4272774cdc947fedda6770648bdfc
erxe <figueroa0cristian@gmail.com>
2022-06-30 14:27:10 -0500

    Implementacion de servicio de guardado de jwt

---
+++ ClientApp/src/app/services/token.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenService {
+
+  constructor() { }
+
+  saveToken(token: string){
+    // document.cookie = `token=${token}`;
+    localStorage.setItem('tokenPetL', token)
+  }
+
+  getToken(){
+    return localStorage.getItem('tokenPetL');
+  }
+}
