3fcc8bf5927bbd2cb193166191f6b2cfaafb917e
erxe <79821846+erx-e@users.noreply.github.com>
2022-08-02 12:33:47 -0500

    Delete environment.prod.ts

--- ClientApp/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  API_URL: 'https://localhost:5001',
-  AWS_REGION: "us-east-1",
-  AWS_KEY_ID: "AKIA5QZLTSGGOBD7U5X5",
-  AWS_KEY_SECRET: "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"
-};
