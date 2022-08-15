6d500d0f292ee1b75a0933c5d2492c3317aa87e7
erox <figueroa0cristian@gmail.com>
2022-08-15 16:19:46 -0500

    Implementacion de date-fns para manejo de fechas con el objetivo de indicar cuanto tiempo ha pasado desde la ultima vez que se vio a la mascota y mejoras en la ui

--- ClientApp/src/app/shared/components/postpet/postpet.component.ts
+++ ClientApp/src/app/shared/components/postpet/postpet.component.ts
@@ -1,18 +1,26 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { postpetView } from 'src/app/models/postpet.model';
+import { Component, OnInit, Input } from "@angular/core";
+import { postpetView } from "src/app/models/postpet.model";
+import { formatDistance, subDays } from "date-fns";
+import es from "date-fns/locale/es";
 
 @Component({
-  selector: 'app-postpet',
-  templateUrl: './postpet.component.html',
-  styleUrls: ['./postpet.component.scss']
+  selector: "app-postpet",
+  templateUrl: "./postpet.component.html",
+  styleUrls: ["./postpet.component.scss"],
 })
 export class PostpetComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   @Input() postpet: postpetView | null;
+  lastTimeSeen: string = null;
 
   ngOnInit(): void {
+    if (this.postpet) {
+      this.lastTimeSeen = formatDistance(
+        new Date(this.postpet.lastTimeSeen),
+        new Date(),
+        { addSuffix: true, locale: es }
+      );
+    }
   }
-
 }
