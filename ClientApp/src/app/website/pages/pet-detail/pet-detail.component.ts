53bd733441b21ac7a5e812ef5e48c248e3d8dea7
erox <figueroa0cristian@gmail.com>
2022-08-17 09:48:10 -0500

    mejora de layout, implementacion de eliminacion y edicion de las publicaciones

--- ClientApp/src/app/website/pages/pet-detail/pet-detail.component.ts
+++ ClientApp/src/app/website/pages/pet-detail/pet-detail.component.ts
@@ -7,6 +7,10 @@ import { UserView } from "src/app/models/user.model";
 import { PostpetService } from "src/app/services/postpet.service";
 import Swiper, { Navigation, Pagination, SwiperOptions } from "swiper";
 import { AuthService } from "src/app/services/auth.service";
+import { of } from "rxjs";
+import format from "date-fns/format";
+import es from "date-fns/locale/es";
+import { LoadingService } from "src/app/services/loading.service";
 
 @Component({
   selector: "app-pet-detail",
@@ -18,44 +22,63 @@ export class PetDetailComponent implements OnInit {
     private router: ActivatedRoute,
     private postpetService: PostpetService,
     private location: Location,
-    private authService: AuthService
+    private authService: AuthService,
+    private loadingService: LoadingService
   ) {}
 
   postpetId: string | null = null;
   postpet: postpetView | null = null;
   user: UserView | null;
+  lastTimeSeen: string = ""
+  notFound: boolean = false;
+  isLoading: boolean = true;
+
+
 
   swiperConfig: SwiperOptions = {
     pagination: true,
     navigation: true,
     slidesPerView: 1,
     spaceBetween: 25,
-  }
+  };
 
-  ngOnInit(): void {
+   ngOnInit(): void {
     this.authService.user$.subscribe((data) => (this.user = data));
-
-    this.router.paramMap
+    this.loadingService.isLoading$.subscribe(isL => this.isLoading = isL)
+     this.router.paramMap
       .pipe(
         switchMap((params) => {
           this.postpetId = params.get("id");
           if (this.postpetId) {
             return this.postpetService.getById(parseInt(this.postpetId));
           }
-          return null;
+          else{
+            
+          }
+          return of(null);
         })
       )
-      .subscribe((data) => (this.postpet = data));
+      .subscribe((data) => {
+        if (data) {
+          this.postpet = data;
+          this.lastTimeSeen = format(new Date(this.postpet.lastTimeSeen), "dd 'de' MMMM 'del' yyyy 'a las' HH:mm", {locale: es})
+        }
+      });
 
-    Swiper.use([Pagination, Navigation])
+    Swiper.use([Pagination, Navigation]);
+
+
+
   }
 
-  deletePost(){
-    this.postpetService.delete(parseInt(this.postpet.id)).subscribe(()=> {
-      this.postpet.urlImgs.
-      this.postpetService.deleteImg()
-      this.location.back()
-    })
+  deletePost() {
+    this.postpetService.delete(parseInt(this.postpet.id)).subscribe(() => {
+      this.postpet.urlImgs.forEach((url) => {
+        let key = url.url.split("/").pop();
+        this.postpetService.deleteImg(key).then(() => {console.log(url.url)});
+      });
+      this.location.back();
+    });
   }
 
   goToBack() {
